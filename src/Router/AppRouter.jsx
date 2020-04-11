5f758101cf8200ab5dec90f1fa9eb887dc20b0c6
jithinlal <jithtitan007@gmail.com>
2020-04-11 20:41:15 +0530

    11/4/2020 - user assign

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -3,27 +3,28 @@ import { useRoutes } from 'hookrouter';
 import NavBar from '../components/Navbars/NavBar';
 import CreateSuspect from '../components/NewSuspect/CreateSuspect';
 import SuspectDetails from '../components/NewSuspect/SuspectDetails';
-
+import UserAssign from '../components/Account/UserAssign';
 
 const routes = {
-    "/": () => <div className="h-screen flex justify-center py-16">Home</div>,
-    "/suspect/create": () => <CreateSuspect/>,
-    "/suspect/details": () => <SuspectDetails/>,
+	'/': () => <div className='h-screen flex justify-center py-16'>Home</div>,
+	'/suspect/create': () => <CreateSuspect />,
+	'/suspect/details': () => <SuspectDetails />,
+	'/create-user': () => <UserAssign />,
 };
 
 const AppRouter = () => {
-    const pages = useRoutes(routes)
-    return(
-        <div>
-            <NavBar />
-            {pages}
-            {!pages && 
-            <div className="h-screen flex justify-center py-16">
-                Error 404: Page not found
-            </div>}
-        </div>
-    )
-}
+	const pages = useRoutes(routes);
+	return (
+		<div>
+			<NavBar />
+			{pages}
+			{!pages && (
+				<div className='h-screen flex justify-center py-16'>
+					Error 404: Page not found
+				</div>
+			)}
+		</div>
+	);
+};
 
-
-export default AppRouter
+export default AppRouter;

c664a33bc24b3ac6228d0d92942df4a291e0ea02
jithinlal <jithtitan007@gmail.com>
2020-04-12 16:46:54 +0530

    11/4/2020 - class changes

--- src/components/Dashboard/view-suspect-details/index.js
+++ src/components/Dashboard/view-suspect-details/index.js
@@ -8,68 +8,129 @@ export default function UserAssign() {
 			<div class='max-w-screen rounded overflow-hidden shadow-lg'>
 				<div class='px-6 py-4 w-screen'>
 					<div class='font-bold text-xl mb-2'>Details</div>
-					<table class='table-auto'>
-						<tbody>
-							<tr>
-								<td class='border px-4 py-2 font-bold bg-grey-lighter'>Name</td>
-								<td class='border px-4 py-2'>Amal</td>
-							</tr>
-							<tr>
-								<td class='border px-4 py-2 font-bold bg-grey-lighter'>Age</td>
-								<td class='border px-4 py-2'>27</td>
-							</tr>
-							<tr>
-								<td class='border px-4 py-2 font-bold bg-grey-lighter'>
-									Gender
-								</td>
-								<td class='border px-4 py-2'>Male</td>
-							</tr>
-							<tr>
-								<td class='border px-4 py-2 font-bold bg-grey-lighter'>
-									Phone number
-								</td>
-								<td class='border px-4 py-2'>+91-1234567890</td>
-							</tr>
-							<tr>
-								<td class='border px-4 py-2 font-bold bg-grey-lighter'>
-									Address
-								</td>
-								<td class='border px-4 py-2'>
-									Somewhere in Kerala, safe and secure, 112976
-								</td>
-							</tr>
-							<tr>
-								<td class='border px-4 py-2 font-bold bg-grey-lighter'>
-									Blood Group
-								</td>
-								<td class='border px-4 py-2'>O +ve</td>
-							</tr>
-							<tr>
-								<td class='border px-4 py-2 font-bold bg-grey-lighter'>DOB</td>
-								<td class='border px-4 py-2'>27/10/1996</td>
-							</tr>
-							<tr>
-								<td class='border px-4 py-2 font-bold bg-grey-lighter'>
-									Nationality
-								</td>
-								<td class='border px-4 py-2'>Indian</td>
-							</tr>
-							<tr>
-								<td class='border px-4 py-2 font-bold bg-grey-lighter'>
-									Disease Status
-								</td>
-								<td class='border px-4 py-2 bg-blue-200'>POSITIVE</td>
-							</tr>
-							<tr>
-								<td class='border px-4 py-2 font-bold bg-grey-lighter'>
-									Source
-								</td>
-								<td class='border px-4 py-2 bg-green-200'>CARE</td>
-							</tr>
-						</tbody>
-					</table>
+					<div class='w-screen'>
+						<div class='px-4 py-5 border-b border-gray-200 sm:px-6'>
+							<h3 class='text-lg leading-6 font-medium text-gray-900'>Amal</h3>
+						</div>
+						<div class='px-4 py-5 sm:px-6'>
+							<dl class='grid grid-cols-1 col-gap-4 row-gap-8 sm:grid-cols-2'>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										Age
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>25</dd>
+								</div>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										Gender
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>Male</dd>
+								</div>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										Address
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>
+										Somewhere in India, safe and secure
+									</dd>
+								</div>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										Phone
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>
+										+91-1234567890
+									</dd>
+								</div>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										DOB
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>
+										12/12/2000
+									</dd>
+								</div>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										Blood Group
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>O +ve</dd>
+								</div>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										Disease Status
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>POSITIVE</dd>
+								</div>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										Source
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>CARE</dd>
+								</div>
+							</dl>
+						</div>
+					</div>
 				</div>
 
+				<div class='px-6 py-4 w-screen'>
+					<div class='font-bold text-xl mb-2'>Nearest facility</div>
+					<div class='w-screen'>
+						<div class='px-4 py-5 border-b border-gray-200 sm:px-6'>
+							<h3 class='text-lg leading-6 font-medium text-gray-900'>
+								Medical college hospital
+							</h3>
+						</div>
+						<div class='px-4 py-5 sm:px-6'>
+							<dl class='grid grid-cols-1 col-gap-4 row-gap-8 sm:grid-cols-2'>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										District
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>Calicut</dd>
+								</div>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										State
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>Kerala</dd>
+								</div>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										Type
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>Hospital</dd>
+								</div>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										Phone
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>
+										+91-1234567890
+									</dd>
+								</div>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										Address
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>ok ok</dd>
+								</div>
+								<div class='sm:col-span-1'>
+									<dt class='text-sm leading-5 font-medium text-gray-500'>
+										Location
+									</dt>
+									<dd class='mt-1 text-sm leading-5 text-gray-900'>
+										here here
+									</dd>
+								</div>
+							</dl>
+						</div>
+					</div>
+				</div>
+
+				<div class='px-6 py-4 w-screen'>
+					<div class='font-bold text-xl mb-2'>Medical History</div>
+				</div>
 				<MedicalHistory />
 			</div>
 		</div>
--- src/components/Dashboard/view-suspect-details/medical-history.js
+++ src/components/Dashboard/view-suspect-details/medical-history.js
@@ -3,71 +3,15 @@ import React from 'react';
 export default function MedicalHistory() {
 	return (
 		<>
-			<div class='w-full md:w-full mx-auto p-8'>
-				<div class='font-bold text-xl mb-2'>Details</div>
-				<div class='shadow-md'>
-					<div class='tab w-full overflow-hidden border-t'>
-						<input
-							class='absolute opacity-0 '
-							id='tab-multi-one'
-							type='checkbox'
-							name='tabs'
-						/>
-						<label
-							class='block p-5 leading-normal cursor-pointer'
-							for='tab-multi-one'>
-							Label One
-						</label>
-						<div class='tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal'>
-							<p class='p-5'>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-								Tenetur, architecto, explicabo perferendis nostrum, maxime
-								impedit atque odit sunt pariatur illo obcaecati soluta molestias
-								iure facere dolorum adipisci eum? Saepe, itaque.
-							</p>
-						</div>
+			<div class='bg-white px-4 py-5 border-b border-gray-200 sm:px-6'>
+				<div class='-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-no-wrap'>
+					<div class='ml-4 mt-2'>
+						<h3 class='text-lg leading-6 font-medium text-gray-900'>Disease</h3>
 					</div>
-					<div class='tab w-full overflow-hidden border-t'>
-						<input
-							class='absolute opacity-0'
-							id='tab-multi-two'
-							type='checkbox'
-							name='tabs'
-						/>
-						<label
-							class='block p-5 leading-normal cursor-pointer'
-							for='tab-multi-two'>
-							Label Two
-						</label>
-						<div class='tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal'>
-							<p class='p-5'>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-								Tenetur, architecto, explicabo perferendis nostrum, maxime
-								impedit atque odit sunt pariatur illo obcaecati soluta molestias
-								iure facere dolorum adipisci eum? Saepe, itaque.
-							</p>
-						</div>
-					</div>
-					<div class='tab w-full overflow-hidden border-t'>
-						<input
-							class='absolute opacity-0'
-							id='tab-multi-three'
-							type='checkbox'
-							name='tabs'
-						/>
-						<label
-							class='block p-5 leading-normal cursor-pointer'
-							for='tab-multi-three'>
-							Label Three
-						</label>
-						<div class='tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal'>
-							<p class='p-5'>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-								Tenetur, architecto, explicabo perferendis nostrum, maxime
-								impedit atque odit sunt pariatur illo obcaecati soluta molestias
-								iure facere dolorum adipisci eum? Saepe, itaque.
-							</p>
-						</div>
+					<div class='ml-4 mt-2 flex-shrink-0'>
+						<p class='relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-dark'>
+							Details of the disease
+						</p>
 					</div>
 				</div>
 			</div>

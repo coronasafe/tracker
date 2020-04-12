d7a3f32b4a75c67ee5f3e8b9649fcc664cb81670
Amal Jose <amaljose96@gmail.com>
2020-04-12 16:43:48 +0530

    Fixed height just for the table

--- src/components/NewSuspect/ExistingSuspectsPopup/index.js
+++ src/components/NewSuspect/ExistingSuspectsPopup/index.js
@@ -4,16 +4,18 @@ import Table from "../../common/Table";
 import { existingSuspectsConfig } from "./config";
 import Button from "../../common/Button";
 
-function ExistingSuspectsPopup({ existingSuspects, onClose ,onOverride}) {
+function ExistingSuspectsPopup({ existingSuspects, onClose, onOverride }) {
   if (existingSuspects.length === 0) {
     return <div />;
   }
   return (
     <Modal onClose={onClose}>
-      <div className="p-8" style={{ maxHeight: "50vh", overflowY: "scroll" }}>
-        <Table rows={existingSuspects} config={existingSuspectsConfig} />
+      <div className="p-8">
+        <div className="p-3" style={{ maxHeight: "50vh", overflowY: "scroll" }}>
+          <Table rows={existingSuspects} config={existingSuspectsConfig} />
+        </div>
         <br />
-        <Button text="Override and create" onClick={onOverride}/>
+        <Button text="Override and create" onClick={onOverride} />
       </div>
     </Modal>
   );

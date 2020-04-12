02f4d758e6f141bf6911417986ddc600532fb189
Gigin George <mail.gigin@gmail.com>
2020-04-12 07:44:00 +0530

    Merge, Add New Header

--- src/components/Navbars/NavBar.jsx
+++ src/components/Navbars/NavBar.jsx
@@ -3,8 +3,9 @@ import { A, navigate } from 'hookrouter'
 
 export default function NavBar(){
   return (
-    <nav class="bg-white px-8 pt-2 shadow-md">
-     <div class="-mb-px flex">
+    <nav className="bg-green-600 px-8 pt-2 shadow-md">
+     <div className="-mb-px flex">
+       <img className="px-10" src={require("../../img/covid_tracker_small_logo.png")} />
        {[
          {
            link: '/',
@@ -17,9 +18,13 @@ export default function NavBar(){
          {
            link: '/suspect/details',
            title: 'Suspect Details'
+         },
+         {
+           link: '/create-user',
+           title: 'Create User'
          }
        ].map(route=>(
-         <A className="no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3 mr-8" 
+         <A className="no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3 mr-8"
                   href={route.link}
           >
              {route.title}
--- src/components/Navbars/PublicNavBar.jsx
+++ src/components/Navbars/PublicNavBar.jsx
@@ -3,8 +3,9 @@ import { A, navigate } from 'hookrouter'
 
 export default function PublicNavBar(){
   return (
-    <nav className="bg-white px-8 pt-2 shadow-md">
+    <nav className="bg-green-600 px-8 pt-2 shadow-md">
      <div className="-mb-px flex">
+       <img className="px-10" src={require("../../img/covid_tracker_small_logo.png")} />
        {[
          {
            link: '/',
@@ -19,7 +20,7 @@ export default function PublicNavBar(){
            title: 'Contact'
          },
        ].map(route=>(
-         <A className="mx-auto no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3" 
+         <A className="mx-auto no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3"
                   href={route.link}
                   key={route.link}>
              {route.title}
